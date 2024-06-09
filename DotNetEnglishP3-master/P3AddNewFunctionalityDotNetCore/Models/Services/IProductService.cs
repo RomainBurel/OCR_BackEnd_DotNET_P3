173cfdfa8427a133b2ba4fddef074f2562a575c3
Romain BUREL <generic3@diademe.fr>
2024-06-09 09:51:46 +0200

    fix: iisue #1 - New product validation - update ProductController.Create method - remove CheckProductModelErrors method from IProductService & ProductService - complete ProductViewModel by adding Required attribute on mandatory fields and input validations attributes - add ProductViewModel.resx files for validation error messages - remove useless ProductService.resx & Product.resx files - add validation tags in Product\Creat.cshtml file

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/Services/IProductService.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/Services/IProductService.cs
@@ -8,14 +8,21 @@ namespace P3AddNewFunctionalityDotNetCore.Models.Services
     public interface IProductService
     {
         List<Product> GetAllProducts();
+
         List<ProductViewModel> GetAllProductsViewModel();
+
         Product GetProductById(int id);
+
         ProductViewModel GetProductByIdViewModel(int id);
+
         void UpdateProductQuantities();
+
         void SaveProduct(ProductViewModel product);
+
         void DeleteProduct(int id);
-        List<string> CheckProductModelErrors(ProductViewModel product);
+
         Task<Product> GetProduct(int id);
+
         Task<IList<Product>> GetProduct();
     }
 }
--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
 
 namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
 {
@@ -7,14 +8,21 @@ namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
         [BindNever]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "ErrorMissingName")]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
         public string Details { get; set; }
 
+        [Required(ErrorMessage = "ErrorMissingStock")]
+        [RegularExpression(@"^-?\d+$", ErrorMessage = "ErrorStockNotAnInteger")]
+        [Range(1, int.MaxValue, ErrorMessage = "ErrorStockNotGreaterThanZero")]
         public string Stock { get; set; }
 
+        [Required(ErrorMessage = "ErrorMissingPrice")]
+        [RegularExpression(@"^-?\d+(\.\d{1,2})?$", ErrorMessage = "ErrorPriceNotANumber")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "ErrorPriceNotGreaterThanZero")]
         public string Price { get; set; }
     }
 }
